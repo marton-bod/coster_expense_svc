08a8749d728603675e27becbdeedb89d26dba8aa
Marton Bod <marton.bod@gmail.com>
2019-01-16 23:46:52 +0100

    Initial commit

---
+++ src/main/java/io/coster/expense_svc/ExpenseSvcApplication.java
@@ -0,0 +1,14 @@
+package io.coster.expense_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSvcApplication.class, args);
+	}
+
+}
+
